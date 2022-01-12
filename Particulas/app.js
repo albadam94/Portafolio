4efd2f903e1cfcb14f35a68124124a70ba56f8ad
Brayan Albadam <blackthorn_1994@hotmail.com>
2022-01-12 01:55:51 -0500

    portafolioHTML

---
+++ Particulas/app.js
@@ -0,0 +1,112 @@
+particlesJS(
+  {
+    "particles": {
+      "number": {
+        "value": 75,
+        "density": {
+          "enable": false,
+          "value_area":1362.9002517356944
+        }
+      },
+      "color": {
+        "value": "#64ffda"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0.8,
+          "color": "#64ffda"
+        },
+        "polygon": {
+          "nb_sides": 4
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.07215354273894853,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1.7053621458328247,
+          "opacity_min": 0.08932849335314796,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 12.181158184520175,
+          "size_min": 5.684540486109416,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 64.13648243462092,
+        "color": "#ffffff",
+        "opacity": 0.6333477640418815,
+        "width": 0.3
+      },
+      "move": {
+        "enable": true,
+        "speed": 4.810236182596568,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 320.6824121731046,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 219.26084732136317,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 1352.1085584817397,
+          "size": 36.54347455356053,
+          "duration": 2.273816194443766,
+          "opacity": 1,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 138.05312609122865,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+);
---
+++ README.txt
@@ -0,0 +1,4 @@
+Pagina web portafolio
+Diseño bajo derecho reservado 
+Albadam Diseñador Multimedia
+Pagina en actualización y cambios
